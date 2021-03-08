b02aa0d7b342ac3219ea4f62b858ca7ec7d219c5
RandomName-proj <62300540+RandomName-proj@users.noreply.github.com>
2021-03-08 19:51:06 +0300

    dc now works

--- m68kAsmParser.gd
+++ m68kAsmParser.gd
@@ -7,13 +7,23 @@ class_name m68kAsmParser
 var parsingLines: PoolStringArray
 var curTextPos:= Vector2(0,0)
 var cmdStartPos:= Vector2(0,0)
+var cmd:= ""
+var cmdSize:= ""
+var rsCounter = 0
+var rawData : PoolByteArray
 
-const stopSymbols: = " .\t\n" # symbols where each command ends
-const gapSymbols: = " \t\n" # symbols which end commands, but are not part of them
+const stopSymbols: = "\t\n " # symbols where each command ends
+const gapSymbols: = "\t\n " # symbols which end commands, but are not part of them
 
+# Name, size in bytes
 enum DataSizes {
-	BYTE,WORD,TRIPLE_BYTE,DOUBLE_WORD,
-	FIVE_BYTES,QUAD_WORD,SIX_WORDS
+	EMPTY = 0
+	BYTE = 1,
+	WORD = 2,
+	TRIPLE_BYTE = 3,
+	DOUBLE_WORD = 4,
+	FIVE_BYTES = 5,
+	QUAD_WORD = 8
 	}
 
 # Called when the node enters the scene tree for the first time.
@@ -21,15 +31,22 @@ func _ready():
 	pass # Replace with function body.
 
 func parse(text: String):
+	cmdStartPos = Vector2(0,0)
+	curTextPos = Vector2(0,0)
+	cmd = ""
+	cmdSize = ""
+	rawData = []
 	parsingLines = text.split("\n")
 #	print(parsingLines)
-	_executeCommand()
-	return text
+	_executeLine()
+	return rawData
 
-func _executeCommand():
-	var cmd = _getNextCommand()
+func _executeLine():
+	_getNextCommand()
+	_executeCommand()
 	print(parsingLines[0])
 	print(cmd)
+	print(cmdSize)
 
 func _getNextCommand():
 	
@@ -47,32 +64,72 @@ func _getNextCommand():
 	
 	cmdStartPos = curTextPos
 	
-	var cmd = parsingLines[cmdStartPos.y].right(cmdStartPos.x)
+	cmd = parsingLines[cmdStartPos.y].right(cmdStartPos.x)
+	
+	var tempCmd = cmd.right(max(cmd.find("."),0))
+	
 	for i in stopSymbols:
-		if (cmd.find(i) != -1):
-			cmd = cmd.substr(cmdStartPos.x-1,cmd.find(i)-cmdStartPos.x+1)
+		if (tempCmd.find(i) != -1):
+				cmd = cmd.substr(cmdStartPos.x-1,tempCmd.find(i)+max(cmd.find("."),0)-cmdStartPos.x+1)
 	
 	curTextPos.x += cmd.length()
-	return cmd
+	
+	if (cmd.find(".") != -1):
+		cmdSize = cmd.right(cmd.find("."))
+		cmd = cmd.left(cmd.find("."))
+	else:
+		cmdSize = ""
+	
+	cmd = cmd.strip_edges()
+	cmdSize = cmdSize.strip_edges()
+	cmdSize.erase(0,1)
+
+func _executeCommand():
+	match cmd:
+		"dc":
+			_executeDC()
+		"rs":
+			_executeRS()
+		
 
 func _getDataSize(sizeText: String):
 	match sizeText:
+		"":
+			return DataSizes.EMPTY
 		"b":
 			return DataSizes.BYTE
 		"w":
 			return DataSizes.WORD
-		"24":
-			return DataSizes.TRIPLE_BYTE
+#		"t":
+#			return DataSizes.TRIPLE_BYTE
 		"l","s":
 			return DataSizes.DOUBLE_WORD
 		"q","d":
 			return DataSizes.QUAD_WORD
-		"x":
-			return DataSizes.SIX_WORDS
 		_: # GDscript alternative of default
 			return null
 
+func _getNumber():
+	return int(parsingLines[curTextPos.y].right(curTextPos.x).strip_edges())
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Data, Size Of Data
+func _writeData(data: int, size: int):
+	if (pow(2,size*8) <= data):
+		print("Error: ", data, " is too high number for ", size, " byte(s)")
+	elif (size != 0):
+		var temp = data
+		for i in range(size):
+			rawData.push_back(temp&0xFF)
+			temp >>= 8
+			print(temp)
+			pass
+	else:
+		print("Warning: Size is 0")
+
+func _executeDC():
+	print(_getNumber())
+	_writeData(_getNumber(), _getDataSize(cmdSize))
+#	print(parsingLines[curTextPos.y].right(curTextPos.x).strip_edges())
+
+func _executeRS():
+	print("RS executed")
